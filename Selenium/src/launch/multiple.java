18fb339bc06d83d0b6fd333636b79761a2348a77
DELL <DELL@DESKTOP-U424R0D>
2022-07-07 21:50:01 +0530

    build

---
+++ Selenium/src/launch/multiple.java
@@ -0,0 +1,12 @@
+package launch;
+
+import java.io.FileInputStream;
+
+public class multiple {
+
+	public static void main(String[] args) {
+		FileInputStream fis new FileInputStream("./)
+
+	}
+
+}
